16268cc1260d4fd76af78f2eb71ce54ce753a110
Justin G <ctuniseji@gmail.com>
2017-09-26 23:32:30 -0500

    adding a footer - but style didn't work on it hmmm

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,7 @@
 import React from 'react';
+import './style.css'
 
-class  Navbar extends React.Component {
+class Navbar extends React.Component {
   render() {
     return(
   <div className="navbar-fixed">
