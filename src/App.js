18a0c9a9815cd702d44ce2c2e82873a596bffc56
Justin G <ctuniseji@gmail.com>
2017-09-26 23:02:05 -0500

    did a collapsebar - took it out

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar';
 
-
 class App extends Component {
   render() {
     return (
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
 const Navbar = () => (
-  <nav>
-    <div className="nav-wrapper">
-      <a href="#" className="brand-logo">Final Project Name</a>
-      <ul id="nav-mobile" className="right hide-on-med-and-down">
-        <li><a href="sass.html">Sass</a></li>
-        <li><a href="badges.html">Components</a></li>
-        <li><a href="collapsible.html">JavaScript</a></li>
-      </ul>
-    </div>
-  </nav>
+  <div className="navbar-fixed">
+    <nav>
+      <div className="nav-wrapper">
+        <a href="#" className="brand-logo">Final Project Name</a>
+        <ul id="nav-mobile" className="right hide-on-med-and-down">
+          <li><a href="">About</a></li>
+          <li><a href="">Sign up</a></li>
+          <li><a href="">Log in</a></li>
+        </ul>
+      </div>
+    </nav>
+  </div>
 );
 
 export default Navbar;
