8f5ce2db76aba2dd950ea975dd8bfa7f4c92a6f1
Justin G <ctuniseji@gmail.com>
2017-10-02 14:51:13 -0500

    more updates

---
+++ test.js
@@ -0,0 +1,14 @@
+/* 
+tournament bracket generator
+    user inputs:
+        title (optional?)
+
+        single elimination  (for now)
+
+        bracket size
+            select bracket size
+            input entrant names
+                GET 4 TO WORK
+
+                */
+
