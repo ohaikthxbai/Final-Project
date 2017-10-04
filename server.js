a2e6c2c3a064860f60d6673cd0add46ae2b1959a
ohaikthxbai <ctuniseji@gmail.com>
2017-10-03 23:26:33 -0500

    commenting out stuff for now

--- server.js
+++ server.js
@@ -2,18 +2,14 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const appRoutes = require('./routes');
+//const appRoutes = require('./routes');
 
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
-app.listen(process.env.PORT || 3001, function(){
-  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
-});
-
-app.use(appRoutes);
+//app.use(appRoutes);
 
 
 // Send every request to the React app
@@ -24,6 +20,6 @@ app.use(appRoutes);
 // ^^ this was written previously somewhere else
 
 
-// app.listen(PORT, function() {
-//   console.log(`🌎 ==> Server now on port ${PORT}!`);
-// });
+app.listen(PORT, function() {
+  console.log(`🌎 ==> Server now on port ${PORT}!`);
+});
