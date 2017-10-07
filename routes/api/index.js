9257ad1b62fcf635315da45cecedd2d1cebdbd98
Justin G <ctuniseji@gmail.com>
2017-10-06 23:27:45 -0500

    heroku

--- routes/api/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const otherRoutes = require("./other");
-
-router.use('/other', otherRoutes);
-
-module.exports = router;
--- server.js
+++ server.js
@@ -2,23 +2,48 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const appRoutes = require('./routes');
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const apiRouter = require("./routes/api");
+const loginRouter = require("./routes/login");
+
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
+// connect to mongoose for an actual database login
+// Database configuration with mongoose
+mongoose.Promise = Promise;
+mongoose.connect("mongodb://localhost/mern");
+const db = mongoose.connection;
+
+// Show any mongoose errors
+db.on("error", function(error) {
+  console.log(`Mongoose Error: ${error}`);
+});
+
+// Once logged in to the db through mongoose, log a success message
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
 
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
 
-app.use(appRoutes);
-
+// connect our API router with the
+// application - these are protected by our
+// JWT!
+app.use(loginRouter);
+app.use("/api", apiRouter);
 
 // Send every request to the React app
 // Define any API routes before this runs
-// app.get("*", function(req, res) {
-//   res.sendFile(path.join(__dirname, "./client/build/index.html"));
-// });
-// ^^ this was written previously somewhere else
-
+app.get("*", function(req, res) {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
 
 app.listen(PORT, function() {
   console.log(`🌎 ==> Server now on port ${PORT}!`);
