60563aec733ed11eb29958b7bad7e8ca5ae717c5
ohaikthxbai <ctuniseji@gmail.com>
2017-10-16 19:48:08 -0500

    callback for auth, build

--- client/src/Auth/auth0-variables.js
+++ client/src/Auth/auth0-variables.js
@@ -1,6 +1,7 @@
 export const AUTH_CONFIG = {
     domain: 'bringit.auth0.com',
     clientId: '-CC2ormUWUT-FpxfNz8VvNU99s1d2M2Y',
-    callbackUrl: 'http://localhost:3000/callback'
+    // callbackUrl: 'http://localhost:3000/callback'
+    callbackUrl: 'https://bring-it-bracket-gen.herokuapp.com/callback'
   }
   
